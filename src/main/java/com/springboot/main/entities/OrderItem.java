3e2fb017a0eda87a5b5906f3f25660d1f2a0b917
n30config <mu-silva@outlook.com>
2021-01-16 11:12:53 -0300

    Não entendi nada dessa aula, meu pai amado

--- src/main/java/com/springboot/main/entities/OrderItem.java
+++ src/main/java/com/springboot/main/entities/OrderItem.java
@@ -1,2 +1,76 @@
-package com.springboot.main.entities;public class OrderItem {
+package com.springboot.main.entities;
+
+import com.springboot.main.entities.pk.OrderItemPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+
+    private Double price;
+
+    public OrderItem(){
+
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
