b01525ac177afc7a76c3afee1d60b3a948395c7d
n30config <mu-silva@outlook.com>
2021-01-18 15:19:58 -0300

    Mensagem personalizada de erro ResourceNotFound

--- src/main/java/com/springboot/main/services/UserService.java
+++ src/main/java/com/springboot/main/services/UserService.java
@@ -2,6 +2,7 @@ package com.springboot.main.services;
 
 import com.springboot.main.entities.User;
 import com.springboot.main.repositories.UserRepository;
+import com.springboot.main.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
